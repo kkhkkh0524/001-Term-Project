ecbf80d81849b46ef2518f1dc77c21ac9a7f7e08
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-11-06 00:56:16 +0900

    Update temp_humidity_sensor.ino

--- temp_humidity_sensor.ino
+++ temp_humidity_sensor.ino
@@ -1,6 +1,20 @@
+/*
+해당 코드를 사용하기 위해선 DHT11 라이브러리가 필요합니다.
+*/
+
+#include "DHT.h"
+#define DHTTYPE DHT11
+
+const int temp_humid_pin = 2;
+
+DHT dht(temp_humid_pin, DHTTYPE);
+
 void setup() {
 }
 
 void loop() {
+  delay(2000);
 
+  int h = dht.readHumidity(); // (단위 : %)
+  int t = dht.readTemperature(); // (단위 : 섭씨)
 }
