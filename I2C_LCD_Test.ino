adee9b5858f29174888aeb662186c731441b771b
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-11-07 01:20:44 +0900

    Update I2C_LCD_Test.ino

--- I2C_LCD_Test.ino
+++ I2C_LCD_Test.ino
@@ -3,14 +3,22 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void setup()
-{
-	lcd.begin();
+int temperature;
+int humidity;
+
+void setup() {
+	lcd.init();
 	lcd.backlight();
-	lcd.print("Hello, world!");
 }
 
-void loop()
-{
-  
+void loop() {
+	lcd.setCursor(0, 0);
+	lcd.print("Temp: ");
+	lcd.print(temperature);
+	lcd.print("C");
+
+	lcd.setCursor(0, 1);
+	lcd.print("Humidity: ");
+	lcd.print(humidity);
+	lcd.print("%");
 }
