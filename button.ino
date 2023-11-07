30ee3c2cf9f4727801c0dd9ff4576918164a93a0
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-11-07 10:16:14 +0900

    Update button.ino

--- button.ino
+++ button.ino
@@ -1,9 +1,27 @@
+/*
+디지털 핀 번호 값은 임시값
+1번 버튼 (D27) : Setting 
+2번 버튼 (D29) : +1 / +10
+3번 버튼 (D31) : -1 / -10
+4번 버튼 (D33) : Save setting 
+*/
 
+const int buttons[] = {27, 29, 31, 33};
 
-
+bool isSetting = false;
 
 void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(buttons[i], INPUT_PULLUP);
+  }
 }
 
 void loop() {
+
+
+if (isSetting) {
+  // enable 2번
+}
+
+
 }
