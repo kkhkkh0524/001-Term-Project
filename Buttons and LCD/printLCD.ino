333cede8dbbf7b51799c7a7ef789ebc6b410aae6
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-11-07 15:05:27 +0900

    Update printLCD.ino

--- Buttons and LCD/printLCD.ino
+++ Buttons and LCD/printLCD.ino
@@ -1,3 +1,31 @@
 /*
+각 모드에 해당하는 LCD 디스플레이를 출력하는 함수를 포함하는 파일입니다.
+
+Requirements for setup() :
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+}
 
 */
+
+#include <Wire.h> 
+#include "LiquidCrystal_I2C.h"
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void print_amount() {
+  lcd.setCursor(0, 0);
+  lcd.print("Amount :");
+  lcd.print(temp_for_amount);
+  lcd.print(" mL");
+}
+
+void print_amount() {
+  lcd.setCursor(0, 0);
+  lcd.print("Cycle :");
+  lcd.print(temp_for_cycle);
+  lcd.print(" hour");
+}
+
