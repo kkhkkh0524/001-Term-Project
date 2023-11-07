b9e25ae56badd95ccd11836b026d317cc5aa867d
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-11-07 16:17:48 +0900

    Update ultrasonic_sensor.ino

--- Alert/ultrasonic_sensor.ino
+++ Alert/ultrasonic_sensor.ino
@@ -1,25 +1,32 @@
 /*
-이 코드는 개별 동작을 가정하고 쓰인 코드이므로 이후 main 코드와 결합 시 적절한 수정이 필요합니다. 
-실제 남은 양을 계산하기 위해 실측값을 통한 추정이 필요합니다. 
-*/
-
-#include "HCSR04.h"
-
-const int LED_pin = 30;
-
-const int trig_pin = 16;
-const int echo_pin = 14;
-HCSR04 hc(trig_pin, echo_pin); // initialisation class HCSR04 (trig pin , echo pin)
+Requirements for setup() :
 
 void setup() {
     pinMode(LED_pin, OUTPUT);
 }
 
-void loop() {
+*/
+
+#include "HCSR04.h"
+
+const int LED_pin = 30;
+const int trig_pin = 16;
+const int echo_pin = 14;
+
+HCSR04 hc(trig_pin, echo_pin); // initialisation class HCSR04 (trig pin , echo pin)
+
+const int crit_level = 25; // 실제 물의 양을 조절해가며 물보충이 필요하다고 판단되는 수위에서 센서의 값으로 정한다.
+
+void setup() {
+    pinMode(LED_pin, OUTPUT);
+}
+
+
+// 이 함수가 호출되는 주기를 millis()를 통해 적절하게 조정해야한다.
+void calculate_level() {
     int distance = hc.dist(); 
-    
-    if (distance > ...) digitalWrite(LED_pin, HIGH); 
+
+    // 지정한 수위보다 더 낮으면 물보충이 필요함을 알린다.
+    if (distance > crit_level) digitalWrite(LED_pin, HIGH); 
     else digitalWrite(LED_pin, LOW);
-        
-    delay(60); // 다른 시스템 동작을 위해 이후 millis()로 변경 필요  
 }
