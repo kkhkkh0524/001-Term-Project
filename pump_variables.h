bb30a3ab669cb6b59c1e012daa3e8dea354e9917
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-11-08 12:46:44 +0900

    Create pump_variables.h

---
+++ pump_variables.h
@@ -0,0 +1,10 @@
+const int pump_pin = 11; // MOSFET과 연결된 디지털 핀 번호
+const int pass_time = 300; // 물이 펌프로부터 출수구까지 도달하는데 걸리는 시간, 해당값은 예측 값이며 실측 후 값 수정 필요 (ms)  
+
+int pump_duration = (watering_amount * 36.1) + pass_time; // 펌프 작동 지속시간 (ms)
+unsigned long interval = watering_cycle * 3600 * 1000; // 펌프 작동 주기 (ms)
+
+unsigned long previous = 0, pump_previous = 0;
+unsigned long current, pump_current;
+
+bool is_active = false;
