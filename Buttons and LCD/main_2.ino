967fe00139322c6b840ea562e2641aa3588310bc
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-11-08 11:29:34 +0900

    Update main_2.ino

--- Buttons and LCD/main_2.ino
+++ Buttons and LCD/main_2.ino
@@ -32,7 +32,7 @@ void setup() {
 
 // 1번 버튼이 눌러졌는지 검사한다. 이 함수는 기본 모드(온습도 표시 화면)일 때만 호출한다. 세팅 모드로 진입하면 이 함수는 호출되지 않는다.)
 void check_for_setting() {
-	
+
   current_state[0] = digitalRead(buttons[0]);
 
   if (current_state[0] == LOW && previous_state[0] == HIGH) { // 버튼이 눌러지면
@@ -52,10 +52,8 @@ void check_for_setting() {
     }
 }
 
-
 // 급수량을 조절하는 모드, 2번 버튼과 3번 버튼의 입력을 같이 검사하여 임시 급수량 값을 바꾼다.
 void setting_for_amount() {
-
   check_for_save(); // 세팅 모드에서 저장 버튼이 눌려졌는지는 항상 검사해야한다. 
   
   for (int i = 1; i < 3; i++) { // for문을 이용하여 2번 3번 버튼의 상태를 같이 읽는다.
@@ -82,7 +80,6 @@ void setting_for_amount() {
 
 // 급수 주기를 조절하는 모드, 2번 버튼과 3번 버튼의 입력을 같이 검사하여 임시 급수주기 값을 바꾼다.
 void setting_for_cycle() {
-
   check_for_save(); // 세팅 모드에서 저장 버튼이 눌려졌는지는 항상 검사해야한다. 
   
   for (int i = 1; i < 3; i++) { // for문을 이용하여 2번 3번 버튼의 상태를 같이 읽는다.
