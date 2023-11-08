5750dd4e30521aa1f148aee41dee2447208d18f0
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-11-08 23:48:41 +0900

    Update alert.h

--- Alert/alert.h
+++ Alert/alert.h
@@ -11,17 +11,27 @@ void setup() {
 const int LED_pin = 30;
 const int trig_pin = 1;
 const int echo_pin = 2;
+
 const int crit_level = 25; // 실제 물의 양을 조절해가며 물보충이 필요하다고 판단되는 수위에서 센서의 값으로 정한다.
+const int hr_interval = 1 * 60 * 1000 // 1분마다 수위 측정 
+
+unsigned long hr_previous = 0;
+unsigned long hr_current = 0;
 
 HCSR04 hc(trig_pin, echo_pin); // initialisation class HCSR04 (trig pin , echo pin)
 
 
-
-// 이 함수가 호출되는 주기를 millis()를 통해 적절하게 조정해야한다.
 void calculate_level() {
-    int distance = hc.dist(); 
+    hr_current = millis();
 
-    // 지정한 수위보다 더 낮으면 물보충이 필요함을 알린다.
-    if (distance > crit_level) digitalWrite(LED_pin, HIGH); 
-    else digitalWrite(LED_pin, LOW);
+    if (hr_current - hr_previous > hr_interval) {
+        hr_previous = hr_current;
+
+        int distance = hc.dist(); 
+
+        // 지정한 수위보다 더 낮으면 물보충이 필요함을 알린다.
+        if (distance > crit_level) digitalWrite(LED_pin, HIGH); 
+        else digitalWrite(LED_pin, LOW);
+        
+    }
 }
