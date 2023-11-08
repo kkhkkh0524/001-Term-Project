ff070eb6408c1a1439fedd0d8e2a468d9bc90c88
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-11-08 12:46:00 +0900

    Update and rename pump.ino to pump.h

--- pump.h
+++ pump.h
@@ -1,5 +1,4 @@
 /*
-이 코드는 개별 동작을 가정하고 쓰인 코드이므로 이후 main 코드와 결합 시 적절한 수정이 필요합니다. 
 사용자 입력 인터페이스에서 적절한 값을 얻어서 watering_cycle, watering_amount에 사용하며, 설정된 값은 임시로 지정한 값입니다.
 
 Requirements for setup() :
@@ -10,9 +9,9 @@ void setup() {
 */
 
 // 아래 두 값은 버튼 입력에서 받아와서 사용해야한다.
-int watering_cycle = 12;  // 펌프 작동 주기 (hour)
-int watering_amount = 100; // 급수량 (mL) 
 
+// int watering_cycle = 12;  // 펌프 작동 주기 (hour)
+// int watering_amount = 100; // 급수량 (mL) 
 
 const int pump_pin = 11; // MOSFET과 연결된 디지털 핀 번호
 const int pass_time = 300; // 물이 펌프로부터 출수구까지 도달하는데 걸리는 시간, 해당값은 예측 값이며 실측 후 값 수정 필요 (ms)  
