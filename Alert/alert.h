7478702cc5a3765eb80f08776345df077c6c8d40
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-11-08 23:36:34 +0900

    Update and rename alert.ino to alert.h

--- Alert/alert.h
+++ Alert/alert.h
@@ -8,19 +8,13 @@ void setup() {
 
 */
 
-#include <HCSR04.h>
-
 const int LED_pin = 30;
 const int trig_pin = 1;
 const int echo_pin = 2;
+const int crit_level = 25; // 실제 물의 양을 조절해가며 물보충이 필요하다고 판단되는 수위에서 센서의 값으로 정한다.
 
 HCSR04 hc(trig_pin, echo_pin); // initialisation class HCSR04 (trig pin , echo pin)
 
-const int crit_level = 25; // 실제 물의 양을 조절해가며 물보충이 필요하다고 판단되는 수위에서 센서의 값으로 정한다.
-
-void setup() {
-    pinMode(LED_pin, OUTPUT);
-}
 
 
 // 이 함수가 호출되는 주기를 millis()를 통해 적절하게 조정해야한다.
