f6e46106a89a9ac8031639b5410558d2dda7f3c5
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-11-08 13:42:29 +0900

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,8 +1,8 @@
 // 하나의 마이크로컨트롤러로 모든 시스템을 제어한다고 가정할 때 사용할 스케치입니다.
 
-#include <Wire.h>		 // I2C 통신을 위한 라이브러리
+#include <Wire.h>                // I2C 통신을 위한 라이브러리
 #include <LiquidCrystal_I2C.h> 	 // LCD 출력을 위한 라이브러리
-#include <DHT.h> 		 // 온습도 센서를 위한 라이브러리
+#include <DHT.h>                 // 온습도 센서를 위한 라이브러리
 
 // 아래의 3개의 전처리기는 ordered 입니다. (센서 및 LCD 오브젝트 중복선언 방지)
 #include <button_variables.h> 	  // 버튼 입력과 관련된 변수
@@ -11,7 +11,7 @@
 
 #include <pump.h>                 // 펌프 작동을 위한 함수 
 #include <check_function.h> 	  // 버튼 입력 확인을 위한 함수
-#include <setting_function.h> 	  // 세팅값 입력을 위한 함수 
+#include <setting_function.h>     // 세팅값 입력을 위한 함수 
 
 
 // 아래 두 변수는 컴파일 시 최초로 초기화되는 값이며 별도의 값 세팅이 없을 경우 이 값을 기본값으로 사용하여 펌프가 작동됩니다.
