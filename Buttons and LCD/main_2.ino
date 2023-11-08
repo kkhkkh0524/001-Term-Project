6639a6a064ddaeaa5db7f785295cec1a4b1e98be
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-11-08 11:28:11 +0900

    Update main_2.ino

--- Buttons and LCD/main_2.ino
+++ Buttons and LCD/main_2.ino
@@ -1,11 +1,11 @@
-#include <Wire.h> // I2C 통신을 위한 라이브러리
-#include <LiquidCrystal_I2C.h> // LCD 출력을 위한 라이브러리
-#include <DHT.h> // 온습도 센서를 위한 라이브러리
+#include <Wire.h>		 // I2C 통신을 위한 라이브러리
+#include <LiquidCrystal_I2C.h> 	 // LCD 출력을 위한 라이브러리
+#include <DHT.h> 		 // 온습도 센서를 위한 라이브러리
 
 // 아래 전처리기의 순서는 바꿀 수 없습니다. (오브젝트 중복선언 방지)
 
-#include <button_variables.h> // 버튼 입력과 관련된 변수
-#include <print_init_screen.h> // 온습도 값 출력을 위한 함수 
+#include <button_variables.h> 	  // 버튼 입력과 관련된 변수
+#include <print_init_screen.h> 	  // 온습도 값 출력을 위한 함수 
 #include <print_setting_screen.h> // 세팅화면 출력을 위한 함수  
 
 
