27187e548439324883afeba118cd7a79f478a129
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-12-04 16:04:20 +0900

    Update alert_variables.h
    불필요한 변수 제거

--- libraries/Variable/alert_variables.h
+++ libraries/Variable/alert_variables.h
@@ -1,10 +1,6 @@
-const int LED_pin = 30;
-const int trig_pin = 1;
-const int echo_pin = 2;
+const int LED_pin = 30; // LED와 연결된 핀
 
-const int hr_interval = 1 * 60 * 1000; // 1분마다 수위 측정 
+const int trig_pin = 1; // Trig와 연결된 핀 
+const int echo_pin = 2; // Echo와 연결된 핀 
 
-unsigned long hr_previous = 0;
-unsigned long hr_current = 0;
-
-int distance = 0;
+int distance = 0; // 센서에서 읽은 값 
