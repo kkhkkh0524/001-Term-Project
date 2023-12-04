f8bc26cc4263076a899d63fea92185636073a177
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-12-04 16:03:16 +0900

    Update alert.h
    주기 없이 계속 측정하는 것으로 바꾼다.

--- libraries/Function/alert.h
+++ libraries/Function/alert.h
@@ -4,17 +4,10 @@ const int crit_level = 25; // 실제 물의 양을 조절해가며 물보충이 
 
 HCSR04 hc(trig_pin, echo_pin); 
 
-/// @brief 설정한 주기에 따라서 수위 값을 측정하며 임계 수위보다 낮으면 LED를 점등한다.
+/// @brief 수위 값을 측정하여 임계 수위보다 낮으면 LED를 점등한다.
 void calculate_level() {
-    hr_current = millis();
+    distance = hc.dist();
 
-    if (hr_current - hr_previous > hr_interval) {
-        hr_previous = hr_current;
-
-        distance = hc.dist(); 
-
-        // 지정한 수위보다 더 낮으면 물보충이 필요함을 알린다.
-        if (distance > crit_level) digitalWrite(LED_pin, HIGH); 
-        else digitalWrite(LED_pin, LOW);
-    }
+    if (distance > crit_level) digitalWrite(LED_pin, HIGH);
+    else digitalWrite(LED_pin, LOW);
 }
