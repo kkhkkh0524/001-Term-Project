daba027508a8aa053ecc7f5a6b9ff9ee9ca9f89b
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-12-12 01:16:33 +0900

    Add files via upload

--- main/main.ino
+++ main/main.ino
@@ -3,41 +3,42 @@
 #include <DHT.h>                 // 온습도 센서를 위한 라이브러리
 #include <HCSR04.h>							 // 초음파 센서를 위한 라이브러리 
 
-// 아래 3개의 전처리기는 순서를 따릅니다. (오브젝트 및 변수 선언)
 #include <button_variables.h> 	  // 버튼 입력과 관련된 변수
 #include <print_init_screen.h> 	  // 온습도 값 출력을 위한 함수 
 #include <print_setting_screen.h> // 세팅화면 출력을 위한 함수  
 
+#include <pump_variables.h>       // 펌프 동작 제어를 위한 변수
 #include <alert.h>                // 물 보충 알림을 위한 함수 
 #include <pump.h>                 // 펌프 작동을 위한 함수 
 #include <check_function.h>       // 버튼 입력 확인을 위한 함수
 #include <setting_function.h>     // 세팅값 입력을 위한 함수 
 
 void setup() {
-  for (int i = 0; i < 4; i++) pinMode(buttons[i], INPUT_PULLUP); // 입력으로 사용할 4개의 버튼은 내장 풀업 저항을 사용
-  pinMode(pump_pin, OUTPUT); // 펌프를 제어할 MOSFET
-	pinMode(LED_pin, OUTPUT);  // 물 보충 알림에 필요한 LED
+  for (int i = 0; i < 4; i++) pinMode(buttons[i], INPUT_PULLUP);
+  pinMode(pump_pin, OUTPUT);
+	pinMode(LED_pin, OUTPUT); 
 	
-  // LCD 초기화 (글자가 보이지 않으면 뒷면의 나사를 조절하여 보이도록 조정) 
+  // LCD 초기화
   lcd.init();
   lcd.backlight();
+
+  // 디버깅 환경 : 5초 주기로 1초 동안 펌프 작동
+  // interval = 1000 * 5; 
+  // pump_duration = 1000; 
+
+  // Serial.begin(9600);
 }
 
-void loop() {   
-	
- 	// 설정 모드인 경우
-	if (is_setting) {
-		if (is_amount_setting) setting_for_amount(); // 급수량을 설정하는 경우 
-		else if (is_cycle_setting) setting_for_cycle(); // 급수 주기를 설정하는 경우
-	} 
-		
- 	// 설정 모드가 아닌 경우
-	else {
-    read_DHT_immediate(); // 온습도 측정 및 화면에 출력 
+void loop() {
+  if (is_active) check_for_pump();
 
-	  check_for_setting();  // 설정 버튼이 눌렸는지 검사
-    check_for_pump();     // 펌프 제어
+  else if (is_setting && is_amount_setting) setting_for_amount();
+  else if (is_setting && is_cycle_setting) setting_for_cycle();
 
-	  calculate_level();    // 물탱크 수위 측정 및 알림 
-  }
+  else {
+      read_DHT_immediate(); // 온습도 측정 및 화면에 출력 
+      check_for_setting();  // 설정 버튼 눌렸는지 검사
+      check_for_pump();     // 펌프 작동 주기 및 작동 시간 계산
+      calculate_level();    // 물탱크 수위 측정 후 알림까지  
+    }
 }
