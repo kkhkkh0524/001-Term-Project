d4a0d93e3d63e2b1286b847004e4bbd3b1cc2374
kkhkkh0524 <127320580+kkhkkh0524@users.noreply.github.com>
2023-12-25 22:00:33 +0900

    Add files via upload

--- main/main.ino
+++ main/main.ino
@@ -1,28 +1,31 @@
 #include <Wire.h>                
 #include <LiquidCrystal_I2C.h> 	 
 #include <DHT.h>            
-#include <HCSR04.h>							
+// #include <HCSR04.h>							
 
 #include <button_variables.h> 	 
 #include <print_init_screen.h> 	 
 #include <print_setting_screen.h> 
 
 #include <pump_variables.h>     
-#include <alert.h>             
+// #include <alert.h>             
 #include <pump.h>                
 #include <check_function.h>      
 #include <setting_function.h>     
 #include <rem_function.h>
-#include <instant_watering.h>
+// #include <instant_watering.h>
 
 void setup() {
   for (int i = 0; i < 4; i++) pinMode(buttons[i], INPUT_PULLUP);
   pinMode(pump_pin, OUTPUT);
-	pinMode(LED_pin, OUTPUT); 
+	// pinMode(LED_pin, OUTPUT); 
 	
   lcd.init();
   lcd.backlight();
 
+  // Serial.begin(9600);
+  digitalWrite(pump_pin, LOW);
+  
   // 디버깅 환경 : 5초 주기로 1초 동안 펌프 작동
   // interval = 1000 * 5; 
   // pump_duration = 1000; 
@@ -35,7 +38,7 @@ void loop() {
   else if (is_setting && is_cycle_setting) setting_for_cycle();
 
   else {
-      instant_watering();
+      // instant_watering();
       check_for_remaining(); 
       read_DHT_immediate(); 
       check_for_setting();  
